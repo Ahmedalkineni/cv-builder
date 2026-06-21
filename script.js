b584e223126507430f93bd4858b0fadbd271fed9
DealCode <testksa5961@gmail.com>
2026-06-21 23:13:02 +0300

    script.js

--- script.js
+++ script.js
@@ -1,65 +1,41 @@
 function generateCV(){
 
-const name = document.getElementById("name").value;
-const jobTitle = document.getElementById("jobTitle").value;
-const phone = document.getElementById("phone").value;
-const email = document.getElementById("email").value;
-const summary = document.getElementById("summary").value;
-const experience = document.getElementById("experience").value;
-const education = document.getElementById("education").value;
+const name =
+document.getElementById("name").value;
 
-const skills = document
-.getElementById("skills")
-.value
-.split("\n")
-.filter(skill => skill.trim() !== "");
+const title =
+document.getElementById("title").value;
 
-const skillList = skills
-.map(skill => `<li>${skill}</li>`)
-.join("");
+const phone =
+document.getElementById("phone").value;
+
+const email =
+document.getElementById("email").value;
+
+const summary =
+document.getElementById("summary").value;
 
 document.getElementById("cv").innerHTML = `
 
 <div class="header">
 
 <div>
-<div class="name">${name}</div>
-<div class="job">${jobTitle}</div>
+<h1>${name}</h1>
+<p>${title}</p>
 </div>
 
 <div class="contact">
-<div>${phone}</div>
-<div>${email}</div>
+<p>${phone}</p>
+<p>${email}</p>
 </div>
 
 </div>
 
-<div class="section">
-<h2>PERSONAL SUMMARY</h2>
+<hr>
+
+<h3>PERSONAL SUMMARY</h3>
+
 <p>${summary}</p>
-</div>
-
-<div class="section">
-<h2>WORK EXPERIENCE</h2>
-<p>${experience}</p>
-</div>
-
-<div class="section">
-<h2>SKILLS</h2>
-<ul>
-${skillList}
-</ul>
-</div>
-
-<div class="section">
-<h2>EDUCATION</h2>
-<p>${education}</p>
-</div>
-
-<div class="section">
-<h2>REFERENCES</h2>
-<p>Available Upon Request</p>
-</div>
 
 `;
 
